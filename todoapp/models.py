f31a174f45ae28c8a4201442e2286fb399b6a721
Abdullahalmuhit <abdullahalmuhit1296@gmail.com>
2019-12-29 13:18:46 +0600

    added design

--- todoapp/models.py
+++ todoapp/models.py
@@ -6,6 +6,8 @@ class ToDo(models.Model):
     name = models.CharField(max_length=50)
     # comment = models.CharField(max_length=500)
     write = models.TextField()
+    posted_on = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True, auto_now_add=False)
     def __str__(self):
         return (self.name)
 
